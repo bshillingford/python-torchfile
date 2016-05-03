8e88873a6f4d18df9cb36b2b174279ae2f83c4ab
Brendan Shillingford <brendan.shillingford@cs.ox.ac.uk>
2016-05-03 12:59:13 +0100

    Put on pypi, add license

--- setup.py
+++ setup.py
@@ -1,23 +1,30 @@
 from distutils.core import setup
 
-version = '0.0.1'
+version = '0.0.2'
 
 setup(
     name = 'torchfile',
     version = version,
     description = "Torch7 binary serialized file parser",
     classifiers = [
-        'Topic :: Software Development :: Libraries',
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved',
         'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Scientific/Engineering',
         'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5'
     ],
     author = 'Brendan Shillingford',
     author_email = 'brendan.shillingford@cs.ox.ac.uk',
     url = 'https://github.com/bshillingford/python-torchfile',
-    license = 'MIT',
+    license = 'BSD',
     py_modules=['torchfile']
 )
 
